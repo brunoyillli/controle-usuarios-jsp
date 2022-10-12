769b955f8437db94118a75b5a191c1d47df51f0d
Bruno Mendes <wolfteam1910@gmail.com>
2022-10-11 22:29:31 -0300

    validando se login ja existe

--- src/main/java/io/github/brunoyillli/controleusuariosjsp/dao/DAOUsuarioRepository.java
+++ src/main/java/io/github/brunoyillli/controleusuariosjsp/dao/DAOUsuarioRepository.java
@@ -9,46 +9,57 @@ import io.github.brunoyillli.controleusuariosjsp.connection.SingleConnectionBanc
 import io.github.brunoyillli.controleusuariosjsp.model.ModelLogin;
 
 public class DAOUsuarioRepository {
-	
+
 	private Connection connection;
-	
+
 	public DAOUsuarioRepository() {
 		connection = SingleConnectionBanco.getConnection();
 	}
 
 	public ModelLogin gravarUsuario(ModelLogin login) throws SQLException {
-		
+
 		String sql = "INSERT INTO model_login(login,senha,nome,email) VALUES (?,?,?,?)";
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		
+
 		preparedStatement.setString(1, login.getLogin());
 		preparedStatement.setString(2, login.getSenha());
 		preparedStatement.setString(3, login.getNome());
 		preparedStatement.setString(4, login.getEmail());
 		preparedStatement.execute();
-		
+
 		connection.commit();
-		
+
 		return this.consultaUsuario(login.getLogin());
-		
+
 	}
-	
+
 	public ModelLogin consultaUsuario(String login) throws SQLException {
 		ModelLogin modelLogin = new ModelLogin();
-		String sql = "SELECT * FROM model_login where upper(login) = upper('"+login+"')";
-		
+		String sql = "SELECT * FROM model_login where upper(login) = upper('" + login + "')";
+
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		
+
 		ResultSet resultado = preparedStatement.executeQuery();
-		
-		while(resultado.next()) {
+
+		while (resultado.next()) {
 			modelLogin.setId(resultado.getLong("id"));
 			modelLogin.setEmail(resultado.getString("email"));
 			modelLogin.setLogin(resultado.getString("login"));
 			modelLogin.setSenha(resultado.getString("senha"));
 			modelLogin.setNome(resultado.getString("nome"));
 		}
-		
+
 		return modelLogin;
+	}
+
+	public boolean validarLogin(String login) throws SQLException {
+		String sql = "SELECT COUNT(1) > 0 as existe FROM model_login WHERE upper(login) = upper('" + login + "')";
+
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		ResultSet resultado = preparedStatement.executeQuery();
+
+		resultado.next();
+		return resultado.getBoolean("existe");
+
 	}
 }
--- src/main/java/io/github/brunoyillli/controleusuariosjsp/servlets/ServletUsuarioController.java
+++ src/main/java/io/github/brunoyillli/controleusuariosjsp/servlets/ServletUsuarioController.java
@@ -17,39 +17,46 @@ public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = -3474133777043285038L;
 
 	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
-	
-	public ServletUsuarioController() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletUsuarioController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-		
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-	
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
-		modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
-		
-		request.setAttribute("mensagem", "Operação realizada com sucesso!");
-		request.setAttribute("modelLogin", modelLogin);
-		
-		RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
-		redireciona.forward(request, response);
-		}catch (Exception e) {
+			String mensagem = "Operação realizada com sucesso!";
+
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+			if (daoUsuarioRepository.validarLogin(modelLogin.getLogin()) && modelLogin.getId() == null) {
+				mensagem = "Já existe usuario com o mesmo login, informe outro login";
+			} else {
+				modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
+			}
+
+			request.setAttribute("mensagem", mensagem);
+			request.setAttribute("modelLogin", modelLogin);
+
+			RequestDispatcher redireciona = request.getRequestDispatcher("principal/usuario.jsp");
+			redireciona.forward(request, response);
+		} catch (Exception e) {
 			e.printStackTrace();
 			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
 			request.setAttribute("mensagemErro", e.getMessage());
